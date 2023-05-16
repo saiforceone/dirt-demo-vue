66c2de477c487877c7f21f8f86af7b6d3fc52ef7
sai <saiforceone@gmail.com>
2023-05-16 17:46:15 -0500

    update hosts

--- dirt_settings/prod.py
+++ dirt_settings/prod.py
@@ -7,7 +7,7 @@ print('Loaded with production configuration...')
 # You may add to or overwrite ALLOWED_HOSTS below.
 # For example, you could add a host to the list without overwriting by doing the following
 # ALLOWED_HOSTS += ['127.0.0.1']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # By default, in production, DEBUG is to be False
 DEBUG = False
